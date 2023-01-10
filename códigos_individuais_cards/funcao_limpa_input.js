b7b595b36fe5df1c7e4c75f2de532e80624506ee
francivaldolima <francivaldolimadesousa@gmail.com>
2023-01-10 14:53:09 -0300

    Função limpa campos input

---
+++ códigos_individuais_cards/funcao_limpa_input.js
@@ -0,0 +1,6 @@
+function limparCamposInput() {
+  document.getElementById("salario").value = "";
+  document.getElementById("dias-trabalhados").value = "";
+  document.getElementById("meses-trabalhados-ano").value = "";
+  document.getElementById("total-de-meses-trabalhados").value = "";
+}
