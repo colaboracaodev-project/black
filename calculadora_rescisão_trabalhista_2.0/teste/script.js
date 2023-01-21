66e6178c0615495eebdcb5d623559c04c6a9cee7
Eduardo Escudero <escudero.ti@gmail.com>
2023-01-21 16:24:51 -0300

    att

--- calculadora_rescisão_trabalhista_2.0/teste/script.js
+++ calculadora_rescisão_trabalhista_2.0/teste/script.js
@@ -1,17 +1,3 @@
-// function main() {
-//     const salarioDigitado = document.getElementById('salario').value;
-//     const dataEntrada = new Date(document.getElementById('start_date').value);
-//     const dataSaida = new Date(document.getElementById('end_date').value);
-//     const diasMes = dataEntrada.getDate() + 1;
-//     const mesesAno = dataSaida.getMonth() + 1;
-//     const totalDeMesesTrabalhados = parseInt(calculaData(dataEntrada, dataSaida));
-//     const recebeAvisoPrevioIndenizado = true;
-//     const possuiFeriasVencidas = true;
-
-//     const resultado = calculaDemissaoSemJustaCausa(salarioDigitado, diasMes, mesesAno, totalDeMesesTrabalhados,recebeAvisoPrevioIndenizado,possuiFeriasVencidas);
-//     console.log(resultado.toFixed(2));
-// }
-
 function main() {
     const salarioDigitado = 1000;
     const dataEntrada = new Date('2020-01-01');
@@ -22,10 +8,11 @@ function main() {
     const recebeAvisoPrevioIndenizado = true;
     const possuiFeriasVencidas = true;
 
-    const total = document.getElementById('total');
     const resultado = calculaDemissaoSemJustaCausa(salarioDigitado, diasMes, mesesAno, totalDeMesesTrabalhados,recebeAvisoPrevioIndenizado,possuiFeriasVencidas);
-    total.innerHTML = resultado.toFixed(2);
+    console.log(resultado.toFixed(2));
 }
+
+main();
 
 function calculaDemissaoSemJustaCausa(salario, diasMes, mesesAno, totalDeMesesTrabalhados,recebeAvisoPrevioIndenizado,possuiFeriasVencidas) {
     console.log(`salario ${salario} diasMes ${diasMes} mesesAno ${mesesAno} mesesTrabalhados ${totalDeMesesTrabalhados}`);
